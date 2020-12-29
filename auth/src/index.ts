b204d8d6200be20554ef517ac0225a4fbcfd61ed
Aslan Mammadrzayev <aslan@muhammadrza.com>
2020-12-29 19:51:22 +0400

    Import routes to index

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,21 @@
 import express from "express";
 import { json } from "body-parser";
 
+import { currentUserRouter } from "./routes/current-user";
+import { signInRouter } from "./routes/sign-in";
+import { signOutRouter } from "./routes/sign-out";
+import { signUpRouter } from "./routes/sign-up";
+
 const app = express();
 
 app.use(json());
+
+app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
+app.use(signUpRouter);
+
+
 
 app.listen(3000, () => {
     console.log('Listening on port 3000');
