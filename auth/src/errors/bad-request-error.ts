d900b2643e715ccf57f506907ca0a5544742b659
Aslan Mammadrzayev <aslan@muhammadrza.com>
2020-12-30 01:44:17 +0400

    Add new bad request handler

---
+++ auth/src/errors/bad-request-error.ts
@@ -0,0 +1,18 @@
+import { CustomError } from "./custom-error";
+
+export class BadRequestError extends CustomError {
+    statusCode: number = 400;
+
+    constructor(public message: string) {
+        super(message);
+        Object.setPrototypeOf(this, BadRequestError.prototype);
+    }
+
+    serializeErrors() {
+        return [
+            {
+                message: this.message
+            }
+        ];
+    }
+}
