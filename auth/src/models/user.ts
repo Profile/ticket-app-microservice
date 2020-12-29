8883fc29fb693720f8ca49899ea61c00935ec529
Aslan Mammadrzayev <aslan@muhammadrza.com>
2020-12-30 00:53:59 +0400

    Add user model

---
+++ auth/src/models/user.ts
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+/**
+ * User attributes.
+ */
+interface IUserAttrs {
+    email: string;
+    password: string;
+}
+
+/**
+ * User document.
+ */
+interface IUserDocument extends IUserAttrs, mongoose.Document {}
+
+/**
+ * User model.
+ */
+interface IUserModel extends mongoose.Model<IUserDocument> {
+    build(attrs: IUserAttrs): IUserDocument;
+}
+
+const userSchema = new mongoose.Schema({
+   email: {
+       type: String,
+       required: true
+   },
+    password: {
+       type: String,
+        required: true,
+    }
+});
+
+userSchema.statics.build = (attrs: IUserDocument) => {
+    return new User(attrs);
+}
+
+const User = mongoose.model<IUserDocument, IUserModel>('User', userSchema);
+
+export { User };
