5ac3bff87b64db1781dfc04d9d631b290a49521e
Aslan Mammadrzayev <aslan@muhammadrza.com>
2020-12-29 21:27:29 +0400

    Add body validation middleware

--- auth/src/routes/sign-up.ts
+++ auth/src/routes/sign-up.ts
@@ -1,9 +1,25 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
+import { body, validationResult } from "express-validator";
 
 const router = Router();
 
-router.post('/api/users/sign-up', (req, res) => {
-    res.send('Hi!')
+const validationRules = [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password').trim()
+        .isLength({ min: 4, max: 22})
+        .withMessage('Password must be between 4 and 22 characters')
+];
+
+router.post('/api/users/sign-up', validationRules, (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()) {
+        return res.status(400).send(errors.array());
+    }
+
+    res.send('OK');
+
+
 });
 
 export { router as signUpRouter };
