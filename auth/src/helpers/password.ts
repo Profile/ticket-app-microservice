6bc1d013caefd16d0ed2a367356adfe2c99f2e7e
Aslan Mammadrzayev <aslan@muhammadrza.com>
2020-12-30 02:17:56 +0400

    Add Password helper for hashing password

--- auth/src/helpers/password.ts
+++ auth/src/helpers/password.ts
@@ -1,20 +1,12 @@
-import { scrypt, randomBytes } from 'crypto';
-import { promisify } from 'util';
-
-const scryptAsync = promisify(scrypt);
+import bcrypt from "bcrypt";
 
 export class Password {
   static async toHash(password: string) {
-    const salt = randomBytes(8).toString('hex');
-    const buf = (await scryptAsync(password, salt, 64)) as Buffer;
-
-    return `${buf.toString('hex')}.${salt}`;
+    const saltRounds = 10;
+    return bcrypt.hashSync(password, saltRounds);
   }
 
-  static async compare(storedPassword: string, suppliedPassword: string) {
-    const [hashedPassword, salt] = storedPassword.split('.');
-    const buf = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer;
-
-    return buf.toString('hex') === hashedPassword;
+  static async compare(plainPassword: string, hashedPassword: string) {
+    return bcrypt.compareSync(plainPassword, hashedPassword);
   }
-}
+
