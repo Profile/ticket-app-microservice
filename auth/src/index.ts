63828fc8bd9854f9029265fe9ebb1637af34fdad
Aslan Mammadrzayev <aslan@muhammadrza.com>
2020-12-29 21:34:56 +0400

    Add error handler middleware

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,8 @@ import { currentUserRouter } from "./routes/current-user";
 import { signInRouter } from "./routes/sign-in";
 import { signOutRouter } from "./routes/sign-out";
 import { signUpRouter } from "./routes/sign-up";
+import { errorHandler } from "./middlewares/error-handler";
+
 
 const app = express();
 
@@ -15,6 +17,7 @@ app.use(signInRouter);
 app.use(signOutRouter);
 app.use(signUpRouter);
 
+app.use(errorHandler);
 
 
 app.listen(3000, () => {
