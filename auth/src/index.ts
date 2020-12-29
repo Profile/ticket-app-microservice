ce13ce473b1c34fb0c5bc3ba0447139c50c3206b
Aslan Mammadrzayev <aslan@muhammadrza.com>
2020-12-30 01:01:46 +0400

    Connect mongo db

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import "express-async-errors";
 import express from "express";
 import { json } from "body-parser";
+import mongoose from "mongoose";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signInRouter } from "./routes/sign-in";
@@ -26,7 +27,21 @@ app.use("*", async () => {
 
 app.use(errorHandler);
 
+const start = async () => {
+    try {
+        await mongoose.connect('mongodb://auth-mongo-srv', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+        console.log('Connected MongoDB');
+    } catch (e) {
+        console.error(e);
+    }
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000');
-});
+    app.listen(3000, () => {
+        console.log('Listening on port 3000');
+    });
+}
+
+start();
