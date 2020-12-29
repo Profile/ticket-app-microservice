d5a486762f312a1db4991905a95734bf7fc9e560
Aslan Mammadrzayev <aslan@muhammadrza.com>
2020-12-29 23:31:21 +0400

    Update name of variable

--- auth/src/routes/sign-up.ts
+++ auth/src/routes/sign-up.ts
@@ -1,23 +1,30 @@
 import { Router, Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 
+import { RequestValidationError } from "../errors/request-validation-error";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
+
 const router = Router();
 
-const validationRules = [
+const validationSchema = [
     body('email').isEmail().withMessage('Email must be valid'),
     body('password').trim()
         .isLength({ min: 4, max: 22})
         .withMessage('Password must be between 4 and 22 characters')
 ];
 
-router.post('/api/users/sign-up', validationRules, (req: Request, res: Response) => {
+router.post('/api/users/sign-up', validationSchema, (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if(!errors.isEmpty()) {
-        return res.status(400).send(errors.array());
+        throw new RequestValidationError(errors.array());
     }
 
-    res.send('OK');
+    throw new DatabaseConnectionError();
+
+    res.send({
+        status: 'OK'
+    })
 
 
 });
