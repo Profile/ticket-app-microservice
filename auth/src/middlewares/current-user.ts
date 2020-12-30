06b3f65ebb44b32a36dc470a0866391d5bea36a5
Aslan Mammadrzayev <aslan@muhammadrza.com>
2020-12-30 23:13:39 +0400

    Add logged in user middleware handler

---
+++ auth/src/middlewares/current-user.ts
@@ -0,0 +1,35 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+/**
+ * Logged user model.
+ * */
+interface IUserPayload {
+    id: string;
+    email: string;
+}
+
+declare global {
+    namespace Express {
+        interface Request {
+            currentUser?: IUserPayload
+        }
+    }
+}
+
+export const currentUser = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    if(!req.session?.jwt) {
+        return next();
+    }
+
+    try {
+        const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!) as IUserPayload;
+        req.currentUser = payload;
+    } catch {}
+
+    next();
+}
