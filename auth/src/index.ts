a5f9e3d515b92fbda14222ff17272867c7b83bc1
Aslan Mammadrzayev <aslan@muhammadrza.com>
2020-12-30 13:24:09 +0400

    Add cookie-session package

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import "express-async-errors";
 import express from "express";
 import { json } from "body-parser";
 import mongoose from "mongoose";
+import cookieSession from "cookie-session";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signInRouter } from "./routes/sign-in";
@@ -13,7 +14,13 @@ import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 
+app.set('trust proxy', true);
+
 app.use(json());
+app.use(cookieSession({
+    signed: false,
+    secure: true,
+}));
 
 app.use(currentUserRouter);
 app.use(signInRouter);
