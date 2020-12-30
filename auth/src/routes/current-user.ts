bdd8ac2505c0dbf727276842e8790227b71f2341
Aslan Mammadrzayev <aslan@muhammadrza.com>
2020-12-31 00:34:29 +0400

    Add current user feature

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
 
+import { currentUser } from "../middlewares/current-user";
+
 const router = Router();
 
-router.get('/api/users/current-user', (req, res) => {
-    res.send('Hi!')
+router.get('/api/users/current-user', currentUser, (req, res) => {
+    res.send({
+        currentUser: req.currentUser || null
+    })
 });
 
 export { router as currentUserRouter };
