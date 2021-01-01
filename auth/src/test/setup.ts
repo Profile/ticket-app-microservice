9378ba2bab62fda254d6a52855cadc7e46e58a61
Aslan <muhammadrza@mail.ru>
2021-01-02 00:04:05 +0400

    Add global fn for signin case

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,8 +1,19 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
+import request from 'supertest';
+
 import { app } from '../app';
 
+declare global {
+    namespace NodeJS {
+        interface Global {
+            signin(): Promise<string[]>;
+        }
+    }
+}
+
 let mongo: any;
+
 beforeAll(async () => {
     process.env.JWT_KEY = 'secret';
 
@@ -27,3 +38,20 @@ afterAll(async () => {
     await mongo.stop();
     await mongoose.connection.close();
 });
+
+global.signin = async () => {
+    const email = 'test@test.com';
+    const password = 'password';
+
+    const response = await request(app)
+        .post('/api/users/signup')
+        .send({
+            email,
+            password
+        })
+        .expect(201);
+
+    const cookie = response.get('Set-Cookie');
+
+    return cookie;
+};
