0a187a4fc658b46628299f12f4ef69f40da937c2
Aslan <muhammadrza@mail.ru>
2021-01-01 23:35:43 +0400

    Add signup test

---
+++ auth/src/routes/__test__/signup.test.ts
@@ -0,0 +1,12 @@
+import request from "supertest";
+import { app } from "../../app";
+
+it('Should return a 201 status on successful signup', async () => {
+    return request(app)
+        .post('/api/users/sign-up')
+        .send({
+            email: 'example@test.com',
+            password: '123456'
+        })
+        .expect(201);
+}, 30000);
