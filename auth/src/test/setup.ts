472343cba257f1c23f719b4154bfda83ba24d7c7
Aslan <muhammadrza@mail.ru>
2021-01-01 23:34:36 +0400

    Update setup file

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,11 +1,11 @@
-import  { MongoMemoryServer } from "mongodb-memory-server";
-import mongoose from "mongoose";
-
-import { app } from "../app";
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import { app } from '../app';
 
 let mongo: any;
-
 beforeAll(async () => {
+    process.env.JWT_KEY = 'secret';
+
     mongo = new MongoMemoryServer();
     const mongoUri = await mongo.getUri();
 
