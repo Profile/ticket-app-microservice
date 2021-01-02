eee82b84fd2ff84fc3b7f822ae8c630513c2a35f
Aslan <muhammadrza@mail.ru>
2021-01-03 01:23:04 +0400

    Rename app

---
+++ tickets/src/index.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+import { app } from "./app";
+
+const start = async () => {
+    try {
+        await mongoose.connect('mongodb://tickets-mongo-srv', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+        console.log('Connected MongoDB');
+    } catch (e) {
+        console.error(e);
+    }
+
+    app.listen(3000, () => {
+        console.log('Listening on port 3000');
+    });
+}
+
+if(!process.env.JWT_KEY) {
+    throw Error('JWT_KEY must be defined');
+}
+
+start();
