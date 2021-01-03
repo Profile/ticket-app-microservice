32a2295538a4c36bf35fa2505e33625d5efd8582
Aslan <muhammadrza@mail.ru>
2021-01-03 13:13:59 +0400

    Update test setup

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -1,57 +1,51 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
-import request from 'supertest';
-
-import { app } from '../app';
+import jwt from 'jsonwebtoken';
 
 declare global {
-    namespace NodeJS {
-        interface Global {
-            signIn(): Promise<string[]>;
-        }
+  namespace NodeJS {
+    interface Global {
+      signin(): string[];
     }
+  }
 }
 
 let mongo: any;
-
 beforeAll(async () => {
-    process.env.JWT_KEY = 'secret';
+  process.env.JWT_KEY = 'secret';
 
-    mongo = new MongoMemoryServer();
-    const mongoUri = await mongo.getUri();
+  mongo = new MongoMemoryServer();
+  const mongoUri = await mongo.getUri();
 
-    await mongoose.connect(mongoUri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    });
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 });
 
 beforeEach(async () => {
-    const collections = await mongoose.connection.db.collections();
+  const collections = await mongoose.connection.db.collections();
 
-    for (let collection of collections) {
-        await collection.deleteMany({});
-    }
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
 });
 
 afterAll(async () => {
-    await mongo.stop();
-    await mongoose.connection.close();
+  await mongo.stop();
+  await mongoose.connection.close();
 });
 
-global.signIn = async () => {
-    const email = 'test@test.com';
-    const password = 'password';
+global.signin = () => {
+  // Build a JWT payload.  { id, email }
+  const payload = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    email: 'test@test.com',
+  };
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+  const session = { jwt: token };
+  const sessionJSON = JSON.stringify(session);
+  const base64 = Buffer.from(sessionJSON).toString('base64');
 
-    const response = await request(app)
-        .post('/api/users/sign-up')
-        .send({
-            email,
-            password
-        })
-        .expect(201);
-
-    const cookie = response.get('Set-Cookie');
-
-    return cookie;
+  return [`session=${base64}`];
 };
